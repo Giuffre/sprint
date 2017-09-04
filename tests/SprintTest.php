838100547cde33cd835556e81e0dcd0c3e3fe9b0
Alberto Villa <alberto.villa@facile.it>
2017-09-04 10:28:31 +0200

    Test parameters in an array

--- tests/SprintTest.php
+++ tests/SprintTest.php
@@ -9,12 +9,25 @@ use PHPUnit\Framework\TestCase;
 
 class SprintTest extends TestCase
 {
-    public function testSprint()
+    public function testSprintWithNamedParameters()
     {
         $result = Sprint::sprint(
             'Le mele sono %s[come] e di colore %s[colore]',
             ['colore' => 'verde'],
             ['come' => 'buone']
+        );
+
+        $this->assertEquals(
+            'Le mele sono buone e di colore verde',
+            $result
+        );
+    }
+
+    public function testSprintWithNamedParametersArray()
+    {
+        $result = Sprint::sprint(
+            'Le mele sono %s[come] e di colore %s[colore]',
+            ['colore' => 'verde', 'come' => 'buone']
         );
 
         $this->assertEquals(
