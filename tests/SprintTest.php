26dd78267966852e313ad24dac8347995ad738b7
Alberto Villa <alberto.villa@facile.it>
2017-09-04 11:00:57 +0200

    Test overridden values

--- tests/SprintTest.php
+++ tests/SprintTest.php
@@ -35,4 +35,32 @@ class SprintTest extends TestCase
             $result
         );
     }
+
+    public function testSprintWithOverriddenNamedValues()
+    {
+        $result = Sprint::sprint(
+            'Le mele sono %s[come] e di colore %s[colore]',
+            ['colore' => 'verde'],
+            ['come' => 'buone'],
+            ['colore' => 'rosso']
+        );
+
+        $this->assertEquals(
+            'Le mele sono buone e di colore rosso',
+            $result
+        );
+    }
+
+    public function testSprintWithOverriddenNamedValuesArray()
+    {
+        $result = Sprint::sprint(
+            'Le mele sono %s[come] e di colore %s[colore]',
+            ['colore' => 'verde', 'come' => 'buone', 'colore' => 'rosso']
+        );
+
+        $this->assertEquals(
+            'Le mele sono buone e di colore rosso',
+            $result
+        );
+    }
 }
