bba29d201b8ec40cb02445bce191610ea57485b8
Alberto Villa <alberto.villa@facile.it>
2017-09-05 10:19:24 +0200

    Test Transformer::transform()

--- tests/Template/TransformerTest.php
+++ tests/Template/TransformerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Template;
 
 use Giuffre\Sprint\Template\NamedValues;
 use Giuffre\Sprint\Template\Template;
+use Giuffre\Sprint\Template\TransformedObject;
 use Giuffre\Sprint\Template\Transformer;
 use Moka\Traits\MokaCleanerTrait;
 use PHPUnit\Framework\TestCase;
@@ -14,13 +15,48 @@ class TransformerTest extends TestCase
 {
     use MokaCleanerTrait;
 
-    public function testConstruct()
+    /**
+     * @var Transformer
+     */
+    private $transformer;
+
+    protected function setUp()
     {
-        $transformer = new Transformer(
-            moka(Template::class),
-            moka(NamedValues::class)
+        $this->transformer = new Transformer(
+            moka(Template::class)->stub([
+                '__toString' => 'one %s[tic] three %s[tac] five'
+            ]),
+            moka(NamedValues::class, 'values')
         );
 
-        $this->assertInstanceOf(Transformer::class, $transformer);
+        moka('values')
+            ->method('getValue')
+            ->willReturnMap([
+                ['tic', 'two'],
+                ['tac', 'four'],
+                ['toe', 'six']
+            ]);
+    }
+
+    public function testConstruct()
+    {
+        $this->assertInstanceOf(Transformer::class, $this->transformer);
+    }
+
+    public function testTransform()
+    {
+        $result = $this->transformer->transform();
+
+        $this->assertInstanceOf(TransformedObject::class, $result);
+
+        $this->assertCount(2, $result->getValues());
+        $this->assertEquals([
+            'two', 'four'
+        ], $result->getValues());
+
+        $this->assertEquals(
+            'one %s three %s five',
+            $result->getTemplate()
+        );
     }
 }
